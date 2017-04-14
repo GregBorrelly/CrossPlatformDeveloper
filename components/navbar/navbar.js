37dca68e4dbb4cf1cd0b41f124a73417f93456c5
backsslash092 <gregborrelly@gmail.com>
2017-04-14 07:47:34 -0400

    Mobile Changes

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -1,7 +1,13 @@
 function openNav() {
-    document.getElementById("myNav").style.width = "30%";
+    document.getElementById("myNav").style.height = "100%";
+    document.getElementById("menuBtn").style.display = "none";
+     document.getElementById("closingX").style.display = "block";
+
 }
 
 function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
+    document.getElementById("myNav").style.height = "0%";
+        document.getElementById("closingX").style.display = "none";
+
+    document.getElementById("menuBtn").style.display = "block";
 }
