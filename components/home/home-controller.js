24ab1fc8c71092767e7f060cf087bfe9b63bfbb2
Greg Borrelly <gregborrelly@gmail.com>
2017-07-29 13:44:56 -0400

    breaking stuff

--- components/home/home-controller.js
+++ components/home/home-controller.js
@@ -4,9 +4,7 @@ angular.module('nycBus').controller("homeController", ['$scope', function($scope
 
         $(document).ready(function(){
             
-             $('.parallax').parallax();
-             $('ul.tabs').tabs();
-            $('.modal').modal();
+    
        
               Typed.new('.element', {
               strings: ["Front End", "Back End"," Full stack", "Cross-Platform Developer"],
